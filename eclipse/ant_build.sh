1d40324198c033b6d94220f96e29b4782380aea6
Anil Putla <anil.putla@gmail.com>
2016-07-30 17:12:57 -0700

    refresh ws

---
+++ eclipse/ant_build.sh
@@ -0,0 +1,3 @@
+export ANT_HOME=/opt/brazil-pkg-cache/packages/ApacheAnt/ApacheAnt-1.7.1.111/X86_LINUX_GCC32_GLIBC23/DEV.STD.PTHREAD/build
+export HT2_BOOTSTRAP_JAR=/opt/brazil-pkg-cache/packages/MiniTrails/MiniTrails-2.0.183.2/X86_LINUX_GCC32_GLIBC23/DEV.STD.PTHREAD/build/lib/miniTrails-2.0.jar
+ant
