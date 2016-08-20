c02dc1698cc07e4d96fa6ee1bd1aecabf0a4c8ca
Anil Putla <anil.putla@gmail.com>
2016-08-20 07:45:59 -0700

    refresh ws

--- src/personal/ubuntu/change_system_settings.sh
+++
@@ -1,5 +0,0 @@
-# Ubuntu 12.*
-gnome-control-center
-
-# Ubuntu 14.*
-unity-control-center
--- src/personal/ubuntu/mount_virt_disk_to_ubuntu.sh
+++
@@ -1,3 +0,0 @@
-dpkg --get-selections | grep sshfs
-sudo apt-get install sshfs
-sshfs -o workaround=rename -o idmap=user $USER@$USER-3.desktop.amazon.com:/rhel5pdi/workplace/projects ~/workplace
