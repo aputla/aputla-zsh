519e8113a9b68cb3e6f513881b8e61ac4bc39bed
Anil Putla <anil.putla@gmail.com>
2016-08-21 11:40:02 -0700

    refresh ws

---
+++ src/unix/git/enable_faster_uploads.sh
@@ -0,0 +1,18 @@
+# Run the following commands on the bare repository (on the server)
+
+git config pack.windowMemory 100m
+git config pack.packSizeLimit 100m
+
+echo "*.pdf -delta" > info/attributes
+echo "*.jpg -delta" > info/attributes
+echo "*.jpeg -delta" >> info/attributes
+echo "*.mts -delta" >> info/attributes
+echo "*.mp4 -delta" >> info/attributes
+echo "*.mov -delta" >> info/attributes
+echo "*.mp3 -delta" >> info/attributes
+echo "*.vob -delta" >> info/attributes
+echo "*.zip -delta" >> info/attributes
+
+git repack -a -d -F --window-memory 100m --max-pack-size 100m
+
+git gc
